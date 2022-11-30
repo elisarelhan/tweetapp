53db0aa79aa9008d6bbeec02310c700b93e8462d
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-12-01 02:27:17 +0530

    Update TweetService.java

--- tweets/src/main/java/com/tweetapp/tweets/service/TweetService.java
+++ tweets/src/main/java/com/tweetapp/tweets/service/TweetService.java
@@ -30,24 +30,7 @@ public class TweetService {
 		return tweetObj;
 	}
 
-	public Tweet getTweet(int tweetId) {
-		if( tweetRepo.findByTweetIdOrderByUpdatedDateDesc(tweetId)==null) {
-			throw new ResourceNotFoundException("Tweet Not Found");
-		} else {
-			return tweetRepo.findByTweetIdOrderByUpdatedDateDesc(tweetId);
-		}
-	}
-
-
-	public List<Tweet> getAllTweets() {
-
-		if (tweetRepo.findAllTweetsOrderByUpdatedDateDesc().isEmpty()) {
-			throw new ResourceNotFoundException("No Tweets Found");
-		} else {
-
-			return tweetRepo.findAll();
-
-
+	
 	public Tweet getTweet(int tweetId) {
 		if (tweetRepo.findByTweetIdOrderByUpdatedDateDesc(tweetId) == null) {
 			templateString.send(USER_CREATED_TOPIC, "Tweet Not Found");
